a5beb96c941817d4331ebace794ce7c2af12bd24
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-02-26 13:10:54 -0500

    Changed comments and created code

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Devon
+ * Created on: Feb 2026
+ * This program will turn a light on and off with buttons. 
 */
 
-basic.showString('Hello, World!')
+
+// clears screen then shows a happy face
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// turns on LED with B button
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+// turns off LED with A button
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0) 
+})
