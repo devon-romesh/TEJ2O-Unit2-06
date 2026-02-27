3fa2362936ee486c08d497f8fcd4aaf3c3ed18a5
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-02-27 17:49:43 +0000

    code

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -6,4 +6,18 @@ This module is a Micro:bit MicroPython program
 
 from microbit import *
 
-display.scroll("Hello, World!")
+
+# startup for the smily face
+display.show(Image.HAPPY)
+pin16.write_digital(0)
+
+
+while True:
+    # turns light on
+    if button_a.is_pressed():
+        pin16.write_digital(1)
+        display.show(Image.YES)
+    # turns light off
+    if button_b.is_pressed():
+        pin16.write_digital(0)
+        display.show(Image.NO)
